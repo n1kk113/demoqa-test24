cf83b5aa9bab4ddcd5691d6caf0808e2aabff33f
Denis Nikitin <150063921+n1kk113@users.noreply.github.com>
2024-01-30 00:47:15 +0300

    Update RegistrationPages.java

--- src/test/java/pages/RegistrationPages.java
+++ src/test/java/pages/RegistrationPages.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selenide.executeJavaScript;
 public class RegistrationPages {
     CalendarComponent calendarComponent = new CalendarComponent();
     TableResults tableResults = new TableResults();
-    private SelenideElement firstNameInput = $("#firstName"),
+    private final SelenideElement firstNameInput = $("#firstName"),
             lastNameInput = $("#lastName"),
             userEmailInput = $("#userEmail"),
             genderWrapper = $("#genterWrapper"),
